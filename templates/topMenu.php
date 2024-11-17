5f77b747d40d1d238c78f935865d895029056afb
Kimberly Jazlin Jiménez <jjimjazlk@gmail.com>
2024-11-17 14:05:14 -0400

    Source Code

---
+++ templates/topMenu.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand text-uppercase" href="index.php">Librería</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="libros.php">Libros</a></li>
+                        <li class="nav-item"><a class="nav-link" href="autores.php">Autores</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contacto.php">Contáctanos</a></li>
+                    </ul>
+                </div>
+            </div>
+</nav>
